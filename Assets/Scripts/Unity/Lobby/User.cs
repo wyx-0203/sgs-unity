2c96714c3214880352fc185ebc79e6e38ebe8e15
wyx <3081879742@qq.com>
2024-02-24 23:18:37 +0800

    1

---
+++ Assets/Scripts/Unity/Lobby/User.cs
@@ -0,0 +1,14 @@
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class User : MonoBehaviour
+// {
+//     // public int ID { get; private set; }
+//     public Text nickName;
+//     public Image character;
+
+//     public async void Init(Model.User model)
+//     {
+//         // ID = model.id;
+//     }
+// }
